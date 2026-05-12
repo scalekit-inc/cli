5a37c96a1b6f137d72858aa33d28e096e6101f76
Saif Ali Shaik <saif.shaik@scalekit.com>
2026-05-12 17:11:54 +0530

    feat: add cfonts banner with chrome style on CLI launch

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,28 @@
+import cfonts from "cfonts";
 import { Command } from "commander";
+
+function showBanner() {
+	if (process.env.NO_COLOR || process.argv.includes("--plain")) {
+		console.log("ScaleKit CLI");
+		return;
+	}
+
+	cfonts.say("ScaleKit", {
+		font: "chrome",
+		align: "left",
+		colors: ["cyan", "white", "blue"],
+		letterSpacing: 1,
+		space: true,
+	});
+}
 
 const program = new Command();
 
 program.name("scalekit").description("Scalekit CLI").version("0.1.0");
+
+program.action(() => {
+	showBanner();
+	program.help();
+});
 
 program.parse();
