be149054278bd3705a185e7311ab01a0b5ea3105
Saif Ali Shaik <saif.shaik@scalekit.com>
2026-05-12 17:15:00 +0530

    feat: add --plain flag to disable colors and styling

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import cfonts from "cfonts";
 import { Command } from "commander";
 
+function isPlain(): boolean {
+	return !!(process.env.NO_COLOR || process.argv.includes("--plain"));
+}
+
 function showBanner() {
-	if (process.env.NO_COLOR || process.argv.includes("--plain")) {
+	if (isPlain()) {
 		console.log("ScaleKit CLI");
 		return;
 	}
@@ -18,7 +22,11 @@ function showBanner() {
 
 const program = new Command();
 
-program.name("scalekit").description("Scalekit CLI").version("0.1.0");
+program
+	.name("scalekit")
+	.description("Scalekit CLI")
+	.version("0.1.0")
+	.option("--plain", "disable colors and styling (also respects NO_COLOR env)");
 
 program.action(() => {
 	showBanner();
